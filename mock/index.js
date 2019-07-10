c9250b6d443f8ace41386a95d9654b48616afd03
Adam Hegedus <limitlessaudio@gmail.com>
2019-07-10 22:12:57 +0200

    Mock - Create ESlint configuration and include as CI step

--- mock/index.js
+++ mock/index.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const app = express();
-
-app.use(cors());
 
 const port = 9999;
+const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Mock service for car log application.'));
 
 function sendFileContent(fileName, res) {
-    fs.readFile(`${fileName}`, (err, data) => {
-        if (err) {
-            res.sendStatus(404);
-            return;
-        }
+  fs.readFile(`${fileName}`, (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+      return;
+    }
 
-        res.send(data);
-    });
+    res.send(data);
+  });
 }
 
 app.get('/test', (req, res) => {
-    res.append('Content-Type', 'application/json');
-    const fileName = 'test.json';
-    sendFileContent(fileName, res);
+  res.append('Content-Type', 'application/json');
+  const fileName = 'test.json';
+  sendFileContent(fileName, res);
 });
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
