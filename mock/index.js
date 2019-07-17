4c9576617648451767c5b17a410ad5041eeb561b
Adam Hegedus <adam_hegedus@epam.com>
2019-07-17 12:56:53 +0200

    Mock - Fix linting issues

--- mock/index.js
+++ mock/index.js
@@ -1,18 +1,7 @@
-import app from './core/app';
 import cors from 'cors';
-// import sendFileContent from './core/sendFileContent';
+import app from './core/app';
 
 const port = 9999;
-// const app = express();
+
 app.use(cors());
-
-// app.get('/', (req, res) => res.send('Mock service for car log application.'));
-
-// app.get('/test', (req, res) => {
-//   res.append('Content-Type', 'application/json');
-//   const fileName = 'data/test.json';
-//   sendFileContent(fileName, res);
-// });
-
-// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
