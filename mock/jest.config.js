5e776311e7fa207ee186ae607c2294c90cf1d6d7
Adam Hegedus <adam.hegedus@yandex.ru>
2019-07-19 14:19:06 +0200

    Update jest coverage calculation

--- mock/jest.config.js
+++ mock/jest.config.js
@@ -19,9 +19,13 @@ module.exports = {
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: null,
+  collectCoverageFrom: [
+    "src/**/*.js",
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
